c990f03eac4ef0644971195917dd69a7f41927b8
Bhupendra Shekhawat <bhupendrashekhawat1108@gmail.com>
2016-09-18 16:07:01 +0530

    Implement next and previous song navignation

--- app/src/main/java/bhupendrashekhawat/me/android/amuzeplayer/PlayActivity.java
+++ app/src/main/java/bhupendrashekhawat/me/android/amuzeplayer/PlayActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,22 +16,25 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 
-import static bhupendrashekhawat.me.android.amuzeplayer.R.id.btnForward;
-import static bhupendrashekhawat.me.android.amuzeplayer.R.id.btnPlay;
-
-public class PlayActivity extends AppCompatActivity  {
+public class PlayActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
 
     // Media Player
     private MediaPlayer mp;
     private MediaManager mediaManager;
     private ImageButton btnPlay;
+    private ImageButton btnPrevious;
+    private ImageButton btnNext;
+    private ImageButton btnLoop;
+    private ImageButton btnShuffle;
+
     private TextView songTitleLabel;
     private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
     private ImageView blurAlbumArt;
     private SeekBar songProgressBar;
-  //  private Utilities utils;
+
+    private int currentSongIndex =0;
+    //  private Utilities utils;
 
     final String SONG_INDEX = "songIndex";
 
@@ -46,6 +48,11 @@ public class PlayActivity extends AppCompatActivity  {
         mp = new MediaPlayer();
         mediaManager = new MediaManager();
         btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+        btnPrevious = (ImageButton) findViewById(R.id.btnPrevious);
+        btnNext = (ImageButton) findViewById(R.id.btnNext);
+        btnLoop = (ImageButton) findViewById(R.id.btnLoop);
+        btnShuffle = (ImageButton) findViewById(R.id.btnShuffle);
+
         songTitleLabel = (TextView) findViewById(R.id.song_title_label);
         // Getting all songs list
         songsList = mediaManager.getPlayList();
@@ -53,6 +60,9 @@ public class PlayActivity extends AppCompatActivity  {
         blurAlbumArt = (ImageView) findViewById(R.id.album_art_blur);
         songProgressBar = (SeekBar) findViewById(R.id.songProgressBar);
 
+
+        //Listeners
+        mp.setOnCompletionListener(this); // Important
 
 
         //Get the blurred Image for background behind the album art
@@ -62,17 +72,79 @@ public class PlayActivity extends AppCompatActivity  {
 
 
         Intent intent = getIntent();
-        final int songIndex = intent.getIntExtra(SONG_INDEX,0);
+        currentSongIndex = intent.getIntExtra(SONG_INDEX,0);
+        // play selected song
+        playSong(currentSongIndex);
 
-        Toast.makeText(getApplicationContext(), "Song Selected is " +songIndex,  Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Song Selected is " + currentSongIndex ,  Toast.LENGTH_SHORT).show();
 
+        /* Set on click listeners for the playback controls */
 
+        /**
+         * Play button click event
+         * */
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playSong(songIndex);
+                // check for already playing
+                if(mp.isPlaying()){
+                    if(mp!=null){
+                        mp.pause();
+                        // Changing button image to play button
+                        btnPlay.setImageResource(R.drawable.btn_play);
+                    }
+                }else{
+                    // Resume song
+                    if(mp!=null){
+                        mp.start();
+                        // Changing button image to pause button
+                        btnPlay.setImageResource(R.drawable.btn_pause);
+                    }
+                }
             }
         });
+
+        /**
+         * Previous button click event
+         * */
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if(currentSongIndex > 0){
+                    playSong(currentSongIndex - 1);
+                    currentSongIndex = currentSongIndex - 1;
+                }else{
+                    // play last song
+                    playSong(songsList.size() - 1);
+                    currentSongIndex = songsList.size() - 1;
+                }
+
+            }
+        });
+
+
+        /**
+         * Next button click event
+         * Plays next song by taking currentSongIndex + 1
+         * */
+        btnNext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // check if next song is there or not
+                if(currentSongIndex < (songsList.size() - 1)){
+                    playSong(currentSongIndex + 1);
+                    currentSongIndex = currentSongIndex + 1;
+                }else{
+                    // play first song
+                    playSong(0);
+                    currentSongIndex = 0;
+                }
+
+            }
+        });
+
     }
 
 
@@ -110,6 +182,8 @@ public class PlayActivity extends AppCompatActivity  {
     }
 
 
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
 
-
+    }
 }
